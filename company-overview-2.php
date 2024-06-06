dda7d3c814acc0dfb317665abd67a12c919b7e27
usama usman <usmanusama4811@gmail.com>
2024-06-06 18:00:39 +0500

    overvire page done

--- company-overview-2.php
+++ company-overview-2.php
@@ -50,14 +50,16 @@
                 <div class="col-lg-4"></div>
                 <div class="col-lg-8 border-start-pad">
                     <h3 class="section_head2">Our <span>Mission</span></h3>
-                    <p class="text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                    <p class="text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been
                         the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
                         of type and scrambled it to make a type specimen book. It has survived not only five centuries,
                         but also the leap into electronic typesetting, remaining essentially unchanged</p>
 
 
                     <h3 class="section_head2">Our <span>vision</span></h3>
-                    <p class="text-start"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                    <p class="text-start"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been
                         the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
                         of type and scrambled it to make a type specimen book.</p>
                 </div>
@@ -69,8 +71,8 @@
                     <span class="home-video-close is-hidden" onclick="videoStop()">&times;</span>
                     <div class="videoWrapper videoWrapper169 js-videoWrapper">
 
-                        <iframe class="videoIframe js-videoIframe" src="" frameborder="0"
-                            allowTransparency="true" allowfullscreen
+                        <iframe class="videoIframe js-videoIframe" src="" frameborder="0" allowTransparency="true"
+                            allowfullscreen
                             data-src="https://player.vimeo.com/video/189360311?autoplay=1&title=0&byline=0&portrait=0&share=0"></iframe>
 
                         <button class="videoPoster js-videoPoster"
@@ -80,6 +82,212 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+
+
+    <section class="bg-white ctaSec-2 overview-team">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-3">
+                    <img src="assets/img/assets/overview-team.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                    <h3>Lorem Ipsum is simply dummy text</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="over-there bg-white bg-padding global-scale">
+        <div class="container" bis_skin_checked="1">
+            <div class="row" bis_skin_checked="1">
+                <h2 class="sectionHead pb-5">Lorem Ipsum&nbsp;is <span>simply dummy text</span></h2>
+                <div class="col-lg-3" bis_skin_checked="1">
+                    <h3 class="over-h3">What We Believe</h3>
+                </div>
+                <div class="col-lg-9 border-start-pad" bis_skin_checked="1">
+                    <h3 class="section_head2">our core value</h3>
+                    <p class="text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                        only five centuries, but also the leap into electronic typesetting, remaining essentially
+                        unchanged</p>
+
+
+                    <section class="realeated-capability">
+                    <div class="row g-5 pt-5" bis_skin_checked="1">
+                        <div class="col-lg-4" bis_skin_checked="1">
+                            <div class="block-box" bis_skin_checked="1">
+                                <img src="assets/img/assets/humilty.png" alt="icon">
+                                <h3>Humility</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" bis_skin_checked="1">
+                            <div class="block-box" bis_skin_checked="1">
+                                <img src="assets/img/assets/huminith.png" alt="icon">
+                                <h3>Humanity </h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" bis_skin_checked="1">
+                            <div class="block-box" bis_skin_checked="1">
+                                <img src="assets/img/assets/integrity.png" alt="icon">
+                                <h3>Integrity</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard
+
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center pt-5">
+                            <div class="col-lg-4" bis_skin_checked="1">
+                                <div class="block-box" bis_skin_checked="1">
+                                    <img src="assets/img/assets/integrity.png" alt="icon">
+                                    <h3>Lorem ipsum</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard
+
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4" bis_skin_checked="1">
+                                <div class="block-box" bis_skin_checked="1">
+                                    <img src="assets/img/assets/integrity.png" alt="icon">
+                                    <h3>Lorem ipsum</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </section>
+                </div>
+            </div>
+
+
+
+        </div>
+    </section>
+
+
+    
+    <section class="over-there bg-white bg-padding global-scale">
+        <div class="container" bis_skin_checked="1">
+            <div class="row" bis_skin_checked="1">
+               
+                <div class="col-lg-3" bis_skin_checked="1">
+                    <h3 class="over-h3">Who We Are</h3>
+                </div>
+                <div class="col-lg-9 border-start-pad" bis_skin_checked="1">
+                    <h3 class="section_head2">Lorem Ipsum is simply dummy</h3>
+                    <p class="text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                        only five centuries, but also the leap into electronic typesetting, remaining essentially
+                        unchanged</p>
+
+
+                    <section class="realeated-capability">
+                    <div class="row g-5 pt-5" bis_skin_checked="1">
+                        <div class="col-lg-4" bis_skin_checked="1">
+                            <div class="block-box" bis_skin_checked="1">
+                                <img src="assets/img/assets/contractvehicles1.png" alt="icon">
+                                <h3>contract vehicles</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard
+                                </p>
+                                <a class="defaultBtn mt-2" href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" bis_skin_checked="1">
+                            <div class="block-box" bis_skin_checked="1">
+                                <img src="assets/img/assets/ethicsandompliance.png" alt="icon">
+                                <h3>ethics and compliance </h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's
+                                </p>
+                                <a class="defaultBtn mt-2" href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" bis_skin_checked="1">
+                            <div class="block-box" bis_skin_checked="1">
+                                <img src="assets/img/assets/sustainability.png" alt="icon">
+                                <h3>sustainability</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard
+
+                                </p>
+                                <a class="defaultBtn mt-2" href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center pt-5">
+                            <div class="col-lg-4" bis_skin_checked="1">
+                                <div class="block-box" bis_skin_checked="1">
+                                    <img src="assets/img/assets/globaling.png" alt="icon">
+                                    <h3>global</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard
+
+                                    </p>
+                                    <a class="defaultBtn mt-2" href="#">Read more</a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4" bis_skin_checked="1">
+                                <div class="block-box" bis_skin_checked="1">
+                                    <img src="assets/img/assets/diversity.png" alt="icon">
+                                    <h3>diversity , equity and
+                                    inclusion</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been
+
+                                    </p>
+                                    <a class="defaultBtn mt-2" href="#">Read more</a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4" bis_skin_checked="1">
+                                <div class="block-box" bis_skin_checked="1">
+                                    <img src="assets/img/assets/contractvehicles1.png" alt="icon">
+                                    <h3>contract vehicles</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard
+
+                                    </p>
+                                    <a class="defaultBtn mt-2" href="#">Read more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center pt-5">
+                            <div class="col-lg-4" bis_skin_checked="1">
+                                <div class="block-box" bis_skin_checked="1">
+                                    <img src="assets/img/assets/partner.png" alt="icon">
+                                    <h3>partner</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard
+
+                                    </p>
+                                    <a class="defaultBtn mt-2" href="#">Read more</a>
+                                </div>
+                            </div>
+                           
+                            
+                        </div>
+                    </div>
+                    </section>
+                </div>
+            </div>
+
+
+
         </div>
     </section>
 
